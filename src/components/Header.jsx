9d9513d3832a8d06664d0383eddb64e5efa2a219
ducdoan1806 <ducdoan1806@gmail.com>
2024-10-16 16:16:49 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
-import { useSelector } from "react-redux";
+import { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useOutside } from "../utils/util";
+import authSlice from "../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [dropdown, setDropdown] = useState(false);
   const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const ref = useRef(null);
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(authSlice.actions.logout());
+    navigate("/auth/login");
+  };
+  useOutside(ref, () => setDropdown(false));
   return (
     <header className="bg-white shadow-md px-4 py-2 sticky top-0 right-0">
       <div className="flex items-center justify-between">
@@ -15,9 +29,28 @@ const Header = () => {
           <button className="bg-gray-200 p-2 rounded-full text-gray-600 hover:bg-gray-300 focus:outline-none">
             <i className="fas fa-bell"></i>
           </button>
-          <button className="ml-4 bg-gray-200 p-2 rounded-full text-gray-600 hover:bg-gray-300 focus:outline-none">
-            <i className="fas fa-user"></i>
-          </button>
+          <div className="ml-4 relative">
+            <button
+              className="bg-gray-200 p-2 rounded-full text-gray-600 hover:bg-gray-300 focus:outline-none"
+              onClick={() => setDropdown(true)}
+            >
+              <i className="fas fa-user"></i>
+            </button>
+            {dropdown && (
+              <ul
+                className="absolute bg-white rounded overflow-hidden shadow-md right-0 top-11"
+                ref={ref}
+              >
+                <li
+                  className="px-4 py-2 text-red-600 hover:bg-gray-100 cursor-pointer text-nowrap text-base"
+                  onClick={handleLogout}
+                >
+                  <i className="fa-solid fa-right-from-bracket mr-2"></i>
+                  Logout
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
       </div>
     </header>
