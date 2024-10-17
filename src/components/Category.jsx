851be031db9abcc92709c4709a8c1f5c13269e07
ducdoan1806 <ducdoan1806@gmail.com>
2024-10-17 12:52:32 +0700

    update

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,8 +5,8 @@ import Loading from "./Loading";
 const Category = () => {
   const { loading, categories } = useSelector((state) => state.category);
   return (
-    <div className="bg-white shadow-md rounded-lg overflow-hidden lg:w-1/4">
-      <div className="px-5 py-3 border-b border-gray-200 flex justify-between items-center">
+    <div className="bg-white shadow-md rounded-lg overflow-hidden w-full lg:w-1/4">
+      <div className="px-5 py-3 border-b border-gray-200 flex justify-between items-center gap-3">
         <h2 className="text-xl font-bold text-gray-800">Categories</h2>
         <div className="flex gap-2">
           <button className="bg-blue-500 text-white px-2 py-1 rounded-md hover:bg-blue-600 text-sm">
--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,16 +1,57 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 const CategoryItem = ({ code, name }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+  const handleIsEdit = (e) => {
+    e.preventDefault();
+    setIsEdit(!isEdit);
+  };
+  const handleIsDelete = (e) => {
+    e.preventDefault();
+    setIsDelete(!isDelete);
+  };
   return (
-    <div className="px-5 py-2 flex justify-between gap-2 text-sm hover:bg-gray-50">
-      <ul className="grid grid-cols-2 w-full ">
-        <li>{code}</li>
-        <li>{name}</li>
-      </ul>
-      <div>
-        <button>
-          <i className="fas fa-pen"></i>
-        </button>
+    <div>
+      <div className="px-5 py-3 flex justify-between gap-2 text-sm hover:bg-gray-50">
+        <ul className="grid grid-cols-2 w-full ">
+          <li>{code}</li>
+          <li>{name}</li>
+        </ul>
+        {isEdit ? (
+          <div className="flex gap-4">
+            <button className="text-red-500">
+              <i className="fas fa-check"></i>
+            </button>
+            <button className="text-blue-500" onClick={handleIsEdit}>
+              <i className="fas fa-xmark"></i>
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-4">
+            <button className="text-blue-500" onClick={handleIsEdit}>
+              <i className="fas fa-pen"></i>
+            </button>
+            <button className="text-red-500" onClick={handleIsDelete}>
+              <i className="fas fa-trash"></i>
+            </button>
+          </div>
+        )}
       </div>
+      {isDelete && (
+        <div className="px-5 py-3 bg-red-100 text-sm flex justify-between gap-4">
+          <p>Do you want delete this category ?</p>
+          <div className="flex  gap-3">
+            <button
+              className="text-blue-600 font-semibold"
+              onClick={handleIsDelete}
+            >
+              Cancel
+            </button>
+            <button className="text-red-600 font-semibold">Delete</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
