2d3ddd788b26e1cda4aadc472f55e4756dcc0b09
ducdoan1806 <ducdoan1806@gmail.com>
2024-10-30 14:30:42 +0700

    update

--- src/app/http.js
+++ src/app/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getCookie } from "../utils/util";
 
-export const API_URL = "http://127.0.0.1:8000/";
+export const API_URL = "http://backend:8000/";
 const http = axios.create({
   baseURL: API_URL,
   headers: {
