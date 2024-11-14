6eea36322c61702d31dc5411205d311eb2c03d2a
ducdoan1806 <ducdoan1806@gmail.com>
2024-11-14 14:56:34 +0700

    update

--- src/components/ContactSetting.jsx
+++ src/components/ContactSetting.jsx
@@ -48,6 +48,7 @@ const ContactSetting = () => {
               name="name"
               value={contact?.name}
               onChange={handleContact}
+              autoFocus={isCreate}
               className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-pink-500 focus:border-pink-500 text-sm"
             />
             <input
